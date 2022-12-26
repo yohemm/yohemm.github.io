34326fec29fa4d080ef5b0c78564fc7ccad31a36
BloodOfFairy <bloodoffairy@gmail.com>
2022-12-26 08:19:04 +0100

    ajout bg saturation

--- bg.js
+++ bg.js
@@ -1,5 +1,5 @@
 
 const backgroundEffect = document.body.querySelector('.bg')
 for(let i = 0; i*500 < document.body.scrollHeight; i++){
-    backgroundEffect.innerHTML += `<span class='filter-${i} filter-bg' style="top: ${document.body.scrollHeight*Math.random()}px; left: ${Math.random() < 0.5?document.body.scrollWidth-500-200*Math.random():200*Math.random()}px; "></span>`
+    backgroundEffect.innerHTML += `<span class='filter-${i} filter-bg' style="top: ${(document.body.scrollHeight-800)*Math.random()}px; left: ${Math.random() < 0.5?document.body.scrollWidth*0.7-200*Math.random():200*Math.random()}px; "></span>`
 }
